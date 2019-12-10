5f2e4c6cd5e76d19f5e246ce7ac28c078cf1851a
minhtam2048 <minhtam2048@gmail.com>
2019-12-10 23:23:11 +0700

    Added cart Toggling

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -4,9 +4,11 @@ import {connect} from 'react-redux';
 import {ReactComponent as Logo} from '../../assets/amazon.svg';
 import {auth} from '../../firebase/firebase.utils'
 import './header.styles.scss';
+import CartIcon from '../cart-icon/cart-icon.component';
+import CartDropdown from '../cart-dropdown/cart-dropdown.component';
 
 
-const Header = ({currentUser}) => (
+const Header = ({currentUser, hidden}) => (
     <div className='header'>
         <Link className='logo-container' to="/">
             <Logo className='logo' />
@@ -21,12 +23,15 @@ const Header = ({currentUser}) => (
                 :
                 (<Link className='option' to='/signin'>SIGN IN</Link>)
             }
+            <CartIcon />
         </div>
+        {hidden ? null : <CartDropdown /> }
     </div>
 );
 
-const mapStateToProps = (state) => ({
-    currentUser: state.user.currentUser
+const mapStateToProps = ({user: {currentUser}, cart: {hidden}}) => ({
+    currentUser,
+    hidden
 })
 
 
