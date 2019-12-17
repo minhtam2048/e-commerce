dac4840ca5cbecc24524e34987b39a19ab63ec76
minhtam2048 <minhtam2048@gmail.com>
2019-12-17 12:51:28 +0700

    Migrating app to use redux-saga instead of redux-thunk and fetched collections with redux-saga

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -2,51 +2,38 @@ import React, {Component} from 'react';
 import {Route} from 'react-router-dom';
 import {connect} from 'react-redux';
 
-import CollectionOverview from '../../components/collection-overview/collection-overview.component';
-import CollectionPage from '../collection/collection.component';
+import CollectionsOverviewContainer, {} from '../../components/collection-overview/collection-overview.container';
 
-import {firestore, convertCollectionsSnapshotToMap} from '../../firebase/firebase.utils';
-import { updateCollections } from '../../redux/shop/shop.actions';
-import WithSpinner from '../../components/with-spinner/with-spinner.component';
+import { fetchCollectionsStart } from '../../redux/shop/shop.actions';
+import CollectionPageContainer from '../collection/collection.container';
 
-const CollectionsOverviewWithSpinner = WithSpinner(CollectionOverview);
-const ColelctionsPageWithSpinner = WithSpinner(CollectionPage);
-
-class ShopPage extends Component {
-    
-    state = {
-        loading: true
-    }
-    unsubscribeFromSnapshot = null;
+class ShopPage extends Component { 
 
     componentDidMount() {
-        const {updateCollections} = this.props;
-        const collectionRef = firestore.collection('collections');
-        this.unsubscribeFromSnapshot = collectionRef.onSnapshot(async snapshot => {
-            const collectionsMap = convertCollectionsSnapshotToMap(snapshot);
-            console.log(collectionsMap);
-            updateCollections(collectionsMap);
-            this.setState({loading: false})
-        });
+        const {fetchCollectionsStart} = this.props;
+        fetchCollectionsStart();
     }
 
     render() {
         const {match} = this.props;
-        const {loading} = this.state;
         return (
             <div className='shop-page'>
-                <Route exact path={`${match.path}`} 
-                    render={(props) => <CollectionsOverviewWithSpinner isLoading={loading} {...props}/>} /> 
+                <Route  
+                    exact path={`${match.path}`}
+                    component={CollectionsOverviewContainer} 
+                /> 
 
-                <Route path={`${match.path}/:collectionId`}
-                    render={(props) => <ColelctionsPageWithSpinner isLoading={loading} {...props}  />} />
+                <Route 
+                    path={`${match.path}/:collectionId`}
+                    component={CollectionPageContainer} 
+                />
             </div>       
         );
     }
 }
 
-const mapDispatchToProps = dispatch =>({
-    updateCollections: collectionsMap => dispatch(updateCollections(collectionsMap))
+const mapDispatchToProps = dispatch => ({
+    fetchCollectionsStart: () => dispatch(fetchCollectionsStart())
 });
 
 export default connect(null, mapDispatchToProps)(ShopPage);
--- src/redux/store.js
+++ src/redux/store.js
@@ -2,8 +2,12 @@ import {createStore, applyMiddleware} from 'redux';
 import logger from 'redux-logger';
 import {persistStore} from 'redux-persist';
 import rootReducer from './root-reducer';
+import createSagaMiddleWare from 'redux-saga';
+import {fetchCollectionsStart} from './shop/shop.sagas';
 
-const middlewares = [logger];
+const sagaMiddleWare = createSagaMiddleWare();
+
+const middlewares = [sagaMiddleWare];
 
 if (process.env.NODE_ENV === 'development') {
     middlewares.push(logger);
@@ -11,6 +15,8 @@ if (process.env.NODE_ENV === 'development') {
 
 export const store = createStore(rootReducer, applyMiddleware(...middlewares));
 
+sagaMiddleWare.run(fetchCollectionsStart);
+
 export const persistor = persistStore(store);
 
-export default {store, persistor};
+export default {store, persistStore};
