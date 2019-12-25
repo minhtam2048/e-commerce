aeb8ec5fd92ffc1f541f2e7795f5bb8157ecc9d8
minhtam2048 <minhtam2048@gmail.com>
2019-12-25 13:59:15 +0700

    Separating SPA and Server and handle Stripe payment method

--- SPA/src/components/stripe-button/stripe-button.component.jsx
+++ SPA/src/components/stripe-button/stripe-button.component.jsx
@@ -1,15 +1,29 @@
 import React from 'react';
 import StripeCheckout from 'react-stripe-checkout';
+import axios from 'axios';
 
-const onToken = token => {
-    console.log(token);
-    alert('Payment successful');
-}
+
 
 const StripeCheckoutButton = ({price}) => {
     const priceForStripe = price * 100;
     const publishableKey = 'pk_test_ASGb5qcqYXV8iLWOHY8SAIWI00WP53HNdd';
     
+    const onToken = token => {
+        axios({
+            url: 'payment',
+            method: 'post',
+            data: {
+                amount: priceForStripe,
+                token
+            }
+        }).then(response => {
+            alert('Payment successful');
+        }).catch(error => {
+            console.log('Error: ' + JSON.parse(error))
+            alert('error happened');
+        })
+    };
+
     return(
         <StripeCheckout 
         label='Pay now'
